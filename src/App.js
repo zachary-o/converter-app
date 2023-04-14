fd5ce057199618a3015b751bfe6df2fa7a3f47bd
Zach <104291766+zachary-o@users.noreply.github.com>
2023-04-14 16:20:16 +0300

    upload currency-converter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Block from "./Block";
+
+import "./index.scss";
+
+const url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
 
 function App() {
+  const [rates, setRates] = useState([]);
+  const [exchangeRates, setExchangeRates] = useState({});
+  const [fromCurrency, setFromCurrency] = useState("PLN");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setRates(json);
+        const convertArrayToObject = (rates) => {
+          return rates.reduce((obj, item) => {
+            obj[item.cc] = item.rate;
+            return obj;
+          }, {});
+        };
+        setExchangeRates(convertArrayToObject(rates));
+      })
+      .catch((error) => console.log(error));
+  }, [rates]);
+
+  const onChangeFromPrice = (value) => {
+    const result =
+      (exchangeRates[fromCurrency] / exchangeRates[toCurrency]) * value;
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+  };
+
+  const onChangeToPrice = (value) => {
+    const result =
+      (exchangeRates[toCurrency] / exchangeRates[fromCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  };
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
